b2140c8df44676c0ab0e1c6a630d6d62267ad251
Elizabeth Chan <lmchan@brynmawr.edu>
2018-07-02 18:21:31 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,15 +1,16 @@
 def fizzbuzz(int)
+  if int % 3 == 0 and int % 5 == 0# if the number int is divisible by 3
+    return "FizzBuzz" # Go fizz
   if int % 3 == 0 # if the number int is divisible by 3
     return "Fizz" # Go fizz
   elsif int % 5 == 0 # if the number int is divisible by 3
     return "Buzz" # Go fizz
-  elsif int % 3 == 0 and int % 5 == 0# if the number int is divisible by 3
-    return "FizzBuzz" # Go fizz
+  
   else 
     return 
   end
 end
  
-puts fizzbuzz(3) # => You should see a return of "Fizz"
-puts fizzbuzz(5) # => You should see a return of nil
+# puts fizzbuzz(3) # => You should see a return of "Fizz"
+# puts fizzbuzz(5) # => You should see a return of nil
 puts fizzbuzz(15)  # => You should get an ArgumentError
