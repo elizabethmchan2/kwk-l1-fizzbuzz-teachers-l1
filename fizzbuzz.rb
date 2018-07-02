808f4cd89c961cdc8eb69418c87b53bbdcda52fa
Elizabeth Chan <lmchan@brynmawr.edu>
2018-07-02 18:17:21 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,8 +2,9 @@ def fizzbuzz(int)
   if int % 3 == 0 # if the number int is divisible by 3
     "Fizz" # Go fizz
   end
-   if int % 3 == 0 # if the number int is divisible by 3
-    "Fizz" # Go fizz
+  if int % 3 == 0 # if the number int is divisible by 3
+    "Buzz" # Go fizz
+  end
 end
  
 # fizzbuzz(3) # => You should see a return of "Fizz"
