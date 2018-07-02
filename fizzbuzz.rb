d3d210346f2d99be07bf47e2e955ba7c6ce05706
Elizabeth Chan <lmchan@brynmawr.edu>
2018-07-02 18:17:54 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -10,6 +10,6 @@ def fizzbuzz(int)
   end
 end
  
-# fizzbuzz(3) # => You should see a return of "Fizz"
+fizzbuzz(3) # => You should see a return of "Fizz"
 # fizzbuzz(5) # => You should see a return of nil
 # fizzbuzz()  # => You should get an ArgumentError
