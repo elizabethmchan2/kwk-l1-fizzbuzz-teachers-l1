2ecb172896a15a16b21851ae07b4d48d9303845b
Elizabeth Chan <lmchan@brynmawr.edu>
2018-07-02 18:18:21 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,7 +2,7 @@ def fizzbuzz(int)
   if int % 3 == 0 # if the number int is divisible by 3
     "Fizz" # Go fizz
   end
-  if int % 3 == 0 # if the number int is divisible by 3
+  if int % 5 == 0 # if the number int is divisible by 3
     "Buzz" # Go fizz
   end
 end
